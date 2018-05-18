1746a1dca0aa8f5a0bd44cd3b4669101c7f9d363
Lasha <31016641+LidzeGIT@users.noreply.github.com>
2018-05-18 16:50:14 +0300

    Update JavaApplication74.java

--- JavaApplication74/src/javaapplication74/JavaApplication74.java
+++ JavaApplication74/src/javaapplication74/JavaApplication74.java
@@ -1,10 +1,29 @@
+public class NewClient {
+    static WebDriver driver;
 
-package javaapplication74;
-
-public class JavaApplication74 {
-
-    public static void main(String[] args) {
-     
+    @Test
+    public void Step1() {
+        WebElement elemet = (new WebDriverWait(driver, 120)).until(ExpectedConditions.presenceOfElementLocated(By.name("Меню")));
+        elemet.click();
     }
-    
+    @Test
+    public void Step2() {
+        Actions bl = new Actions(driver);
+        bl.moveToElement(driver.findElement(By.name("Новый клиент"))).click().perform();
+    }
+    @Test
+    public void Step3() {
+        Actions bl = new Actions(driver);
+        bl.moveToElement(driver.findElement(By.name("Клиент"))).click().perform();
+    }
+    @Test
+    public void setSurname() {
+         WebElement elemet4 = (new WebDriverWait(driver, 5)).until(ExpectedConditions.presenceOfElementLocated(By.id("134")));
+         elemet4.sendKeys("Loool");
+    }
+    @Test
+    public void setName() {
+       WebElement element5 = (new WebDriverWait(driver, 5)).until(ExpectedConditions.presenceOfElementLocated(By.id("135")));
+       element5.sendKeys("Lalala");
+    }
 }
