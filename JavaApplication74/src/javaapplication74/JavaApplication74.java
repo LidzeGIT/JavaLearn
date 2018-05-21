06ee16f94121ddf03f9ef2c93be2ef7e7ef76662
Lasha <31016641+LidzeGIT@users.noreply.github.com>
2018-05-21 15:36:19 +0300

    Update JavaApplication74.java

--- JavaApplication74/src/javaapplication74/JavaApplication74.java
+++ JavaApplication74/src/javaapplication74/JavaApplication74.java
@@ -1,29 +1,48 @@
-public class NewClient {
-    static WebDriver driver;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import java.util.concurrent.TimeUnit;
+
+public class Chrome {
+
+    private static WebDriver driver;
+
+    @BeforeClass
+    public static void starterChrome() {
+        System.setProperty("webdriver", "C:\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://habr.com/");
+    }
 
     @Test
-    public void Step1() {
-        WebElement elemet = (new WebDriverWait(driver, 120)).until(ExpectedConditions.presenceOfElementLocated(By.name("Меню")));
-        elemet.click();
+    public void reg() {
+        WebElement reg = driver.findElement(By.xpath("/html/body/div[3]/div[2]/div/div/div[2]/a[2]"));
+        reg.click();
+        WebElement email = driver.findElement(By.xpath("//*[@id=\"register_form\"]/div[3]/div/input"));
+        email.sendKeys("lm123123@yandex.ru");
+        WebElement login = driver.findElement(By.xpath("//*[@id=\"register_form\"]/div[4]/div/input"));
+        login.sendKeys("ildfjajfjs");
+        WebElement pass = driver.findElement(By.xpath("//*[@id=\"register_form\"]/div[5]/div/input"));
+        pass.sendKeys("31233123q");
+        WebElement pass2 = driver.findElement(By.xpath("//*[@id=\"register_form\"]/div[6]/div/input"));
+        pass2.sendKeys("31233123q");
+        WebElement auth = driver.findElement(By.xpath("//*[@id=\"register_form\"]/div[7]/div/label/span[1]"));
+        auth.click();
+        WebElement live = driver.findElement(By.xpath("/html/body/div[2]/div[1]/div/a[2]"));
+        live.click();
+
     }
-    @Test
-    public void Step2() {
-        Actions bl = new Actions(driver);
-        bl.moveToElement(driver.findElement(By.name("Новый клиент"))).click().perform();
-    }
-    @Test
-    public void Step3() {
-        Actions bl = new Actions(driver);
-        bl.moveToElement(driver.findElement(By.name("Клиент"))).click().perform();
-    }
-    @Test
-    public void setSurname() {
-         WebElement elemet4 = (new WebDriverWait(driver, 5)).until(ExpectedConditions.presenceOfElementLocated(By.id("134")));
-         elemet4.sendKeys("Loool");
-    }
-    @Test
-    public void setName() {
-       WebElement element5 = (new WebDriverWait(driver, 5)).until(ExpectedConditions.presenceOfElementLocated(By.id("135")));
-       element5.sendKeys("Lalala");
+
+    @AfterClass
+    public static void ret() {
+        WebElement exitButton = driver.findElement(By.xpath("/html/body/div[2]/div[1]/div/a[1]"));
+        exitButton.click();
+        driver.quit();
     }
 }
