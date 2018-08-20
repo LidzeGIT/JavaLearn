e60a11691cd2615afbe1d7ef97d9d51f046e8e46
Lasha <31016641+LidzeGIT@users.noreply.github.com>
2018-08-20 17:41:39 +0300

    Update JavaApplication74.java

--- JavaApplication74/src/javaapplication74/JavaApplication74.java
+++ JavaApplication74/src/javaapplication74/JavaApplication74.java
@@ -1,64 +1,40 @@
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
-import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
+  @Test
+    public void GetCardsRestOwner() {
+        baseURI = getProperty("RBA.Services.Cards.Rest");
+        basePath = getProperty("restGetOwner");
+        authentication =
+                ntlm(getProperty("username"),
+                        getProperty("password"),
+                        null, getProperty("domain"));
+        defaultParser = Parser.JSON;
 
-import java.util.concurrent.TimeUnit;
+        Response response = when().get();
+        String jsonResponse = response.jsonPath().getString("number[11]");
+        Assert.assertEquals("что проверяем", jsonResponse);
+        System.out.println(jsonResponse);
 
-public class Chrome {
+    }
+@Test
+    public void PutBlockCardsRest(){
+        baseURI = getProperty("RBA.Services.Cards.Rest");
+        basePath = getProperty("PostBlockcardsRest");
+        authentication =
+                ntlm(getProperty("username"),
+                        getProperty("password"),
+                        null, getProperty("domain"));
 
-    private static WebDriver driver;
-    private static WebElement element;
-    
-    @BeforeClass
-    public static void starterChrome() {
-        System.setProperty("webdriver", "C:\\sel\\chromedriver.exe");
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.get("https://habr.com/");
-    }
+        defaultParser = Parser.JSON;
+        Response r = given()
+                .contentType("application/json")
+                .body("{" +
+                        "\"blockTypeId\" :" + 5 +","+
+                        " \"blockReasonId\":" + 23 +
+                        "}")
+                .when()
+                .put();
 
-    @Test
-    public void test1() {
-        element = driver.findElement(By.xpath("/html/body/div[3]/div[2]/div/div/div[2]/a[2]"));
-        element.click();
-    }
-    @Test
-    public void test2(){
-        element = driver.findElement(By.xpath("//*[@id=\"register_form\"]/div[3]/div/input"));
-        element.sendKeys("lm123123@yandex.ru");
-    }
-    @Test
-    public void test3(){
-        element = driver.findElement(By.xpath("//*[@id=\"register_form\"]/div[4]/div/input"));
-        element.sendKeys("ildfjajfjs");
-    }
-    @Test
-    public void test4(){
-        element = driver.findElement(By.xpath("//*[@id=\"register_form\"]/div[5]/div/input"));
-        element.sendKeys("31233123q");
-        element = driver.findElement(By.xpath("//*[@id=\"register_form\"]/div[6]/div/input"));
-        element.sendKeys("31233123q");
-    }
-    @Test
-    public void test5(){
-        element = driver.findElement(By.xpath("//*[@id=\"register_form\"]/div[7]/div/label/span[1]"));
-        element.click();
-    }
-    @Test
-    public void test6(){
-        element = driver.findElement(By.xpath("/html/body/div[2]/div[1]/div/a[2]"));
-        element.click();
-    }
+        String body = r.getBody().asString();
+        System.out.println("Response Body:\n " + body);
+        Assert.assertEquals(body,"{\"isSuccess\":false,\"message\":\"Карта постоянно заблокирована\"}");
 
-    @AfterClass
-    public static void ret() {
-        WebElement exitButton = driver.findElement(By.xpath("/html/body/div[2]/div[1]/div/a[1]"));
-        exitButton.click();
-        driver.quit();
     }
-}
